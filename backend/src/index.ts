7129e7c86f44ab4b0027b98091a3272548f9ced0
Harshal Solanki <harshalsolanki4040@gmail.com>
2024-05-27 23:07:49 +0530

    Added render frontend url

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin: 'https://booking-com-4yv6.onrender.com/',
     credentials: true,
 }))
 
